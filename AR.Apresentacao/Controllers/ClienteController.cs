85fd53ac694c76f494968396241d945b6093b2bf
Saulo Costa <79532045+saulomlcosta@users.noreply.github.com>
2022-01-07 16:20:08 -0300

    End Api Test

---
+++ AR.Apresentacao/Controllers/ClienteController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using AR.Data.Interfaces;
+using AR.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AR.Apresentacao.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        private readonly IClienteRepository _repository;
+
+        public ClienteController(IClienteRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_repository.GetAll());
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Add(Cliente cliente)
+        {
+            try
+            {
+                await _repository.Add(cliente);
+                return StatusCode(201);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ AR.Data/Implementation/ClienteRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AR.Data.Interfaces;
+using AR.Domain;
+
+namespace AR.Data.Implementation
+{
+    public class ClienteRepository : IClienteRepository
+    {
+        private readonly ContextoPrincipal _db;
+
+        public ClienteRepository(ContextoPrincipal db)
+        {
+            _db = db;
+        }
+
+        public async Task Add(Cliente entity)
+        {
+            await _db.AddAsync(entity);
+            await _db.SaveChangesAsync();
+
+        }
+
+        public IQueryable<Cliente> GetAll()
+        {
+            return _db.Cliente;
+        }
+    }
+}
